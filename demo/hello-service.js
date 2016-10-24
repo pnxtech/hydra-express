8f1aed015d627ad8353c5f448ef5ac38221db490
cjus <Carlos.Justiniano@gmail.com>
2016-10-23 20:28:40 -0400

    simplified service example

--- demo/hello-service.js
+++ demo/hello-service.js
@@ -1,18 +1,14 @@
-'use strict';
-
 const config = require('./properties').value;
 const version = require('../package.json').version;
 const hydraExpress = require('../index');
 
-config.hydra.serviceName = 'green-service';
-
 hydraExpress.init(config, version, () => {
   hydraExpress.registerRoutes({
-    '/v1/green': require('./hello-v1-api')
+    '/v1/hello/': require('./hello-v1-api')
   });
 })
   .then((serviceInfo) => {
-    // console.log('serviceInfo', serviceInfo);
+	  console.log('serviceInfo', serviceInfo);
   })
   .catch((err) => {
     console.log('err', err);
